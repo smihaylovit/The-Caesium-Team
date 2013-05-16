89fef8ff49f4aa6ff5cb302019fc6f0a1242d6d5
Adrian Hristov <adrianhr91@gmail.com>
2013-05-16 22:15:05 +0300

    complete unit test template
    ready for some unit testing

--- Caesium.BullsAndCows/Caesium.BullsAndCows.Tests/GameEngineTest.cs
+++ Caesium.BullsAndCows/Caesium.BullsAndCows.Tests/GameEngineTest.cs
@@ -10,6 +10,7 @@ namespace Caesium.BullsAndCows.Tests
         private TextWriter consoleWriter = File.CreateText("../../output.txt");
         private TextReader consoleReader;
         private StreamWriter commandWriter = new StreamWriter("../../input.txt");
+        private StreamWriter expectedWriter = new StreamWriter("../../expected.txt");
 
         // This method runs BEFORE every test method.
         [TestInitialize]
@@ -38,9 +39,30 @@ namespace Caesium.BullsAndCows.Tests
         [TestMethod]
         public void RunMethodExitTest()
         {
+            // give commands to the game
             WriteCommand("1234", "y", "exit");
+
+            // for some reason it doesn't work if we just define a string
+            // so we use a txt file to write the expected output
+            using (expectedWriter)
+            {
+                expectedWriter.WriteLine(@"Welcome to ""Bulls and Cows"" game. Please try to guess my secret 4-digit number.
+Use 'top' to view the top scoreboard, 'restart' to start a new game, 'help' to cheat
+and 'exit' to quit the game.
+Enter your guess or command: HOLYCOW, YOU HAVE WON!
+Congratulations! You guessed the secret number in 1 attempts.
+Please enter your name for the top scoreboard: ---------- Scoreboard ----------
+1. y ----> 1 guesses.
+--------------------------------
+
+Do you want to start a new game? (y/n)
+Good bye!");
+            }
+
+            // load the expected output into a string
             string expected = new StreamReader("../../expected.txt").ReadToEnd();
 
+            // start the game
             ScoreBoard currentScoreBoard = new ScoreBoard();
             while (new GameEngine(currentScoreBoard, ScoreBoard.ShowScoreBoard).Run()){ }
 
