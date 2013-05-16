267781948dbd7340fbad45702f7f3f4598f1051d
Adrian Hristov <adrianhr91@gmail.com>
2013-05-16 18:12:57 +0300

    moved the ShowScoreBoard method
    moved from Main class to ScoreBoard class

--- Caesium.BullsAndCows/Caesium.BullsAndCows/ScoreBoard.cs
+++ Caesium.BullsAndCows/Caesium.BullsAndCows/ScoreBoard.cs
@@ -16,6 +16,49 @@ namespace Caesium.BullsAndCows
             this.TopRecords = new List<ScoreRecord>(topRecordsNumber);
         }
 
+        public static void ShowScoreBoard(GameEngine currentGame, ScoreBoard currentScoreBoard)
+        {
+            if (currentGame.numberOfCheats != 0)
+            {
+                Console.WriteLine("Congratulations! You guessed the secret number in " + currentGame.numberOfGuesses +
+                              " attempts and " + currentGame.numberOfCheats + " cheats.");
+                Console.WriteLine("You are not allowed to enter the top scoreboard.");
+                Console.WriteLine(currentScoreBoard.ToString());
+            }
+            else
+            {
+                if (currentScoreBoard.TopRecords.Count < 5)
+                {
+                    Console.WriteLine("Congratulations! You guessed the secret number in " + currentGame.numberOfGuesses + " attempts.");
+                    Console.Write("Please enter your name for the top scoreboard: ");
+
+                    string name = Console.ReadLine();
+
+                    currentScoreBoard.TopRecords.Add(new ScoreRecord(name, currentGame.numberOfGuesses));
+                    currentScoreBoard.TopRecords.Sort();
+                    Console.WriteLine(currentScoreBoard.ToString());
+                }
+                else if (currentGame.numberOfGuesses >= currentScoreBoard.TopRecords[currentScoreBoard.TopRecords.Count - 1].Score)
+                {
+                    Console.WriteLine("Congratulations! You guessed the secret number in " + currentGame.numberOfGuesses + " attempts.");
+                    Console.WriteLine("You are not allowed to enter the top scoreboard.");
+                    Console.WriteLine(currentScoreBoard.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed the secret number in " + currentGame.numberOfGuesses + " attempts.");
+                    Console.Write("Please enter your name for the top scoreboard: ");
+
+                    string name = Console.ReadLine();
+
+                    currentScoreBoard.TopRecords.RemoveAt(currentScoreBoard.TopRecords.Count - 1);
+                    currentScoreBoard.TopRecords.Add(new ScoreRecord(name, currentGame.numberOfGuesses));
+                    currentScoreBoard.TopRecords.Sort();
+                    Console.WriteLine(currentScoreBoard.ToString());
+                }
+            }
+        }
+
         public override string ToString()
         {
             if (this.TopRecords.Count == 0)
