68c2d8eac4a83c6f4e0087c008a5eadb04ba0cd4
Adrian Hristov <adrianhr91@gmail.com>
2013-05-16 19:41:48 +0300

    Added Test Initialize method
    redirecting the console to output data in a file so we can compare the
    output of the game.

---
+++ Caesium.BullsAndCows/Caesium.BullsAndCows.Tests/GameEngineTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Caesium.BullsAndCows.Tests
+{
+    [TestClass]
+    public class GameEngineTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            // redirect the console output to a file so we can compare them.
+            TextWriter writer = File.CreateText("../../output.txt");
+            Console.SetOut(writer);
+        }
+
+        [TestMethod]
+        public void RunMethodTest()
+        {
+
+        }
+    }
+}
